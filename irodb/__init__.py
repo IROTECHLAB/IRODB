d0602223c2cbc3f5cf54071bb1dab93ea2490292
ɪʀσηϻᴧη <iroboy1117@gmail.com>
2026-07-16 09:57:28 +0530

    Upload via IroTech Uploader - irodb/__init__.py

---
+++ irodb/__init__.py
@@ -0,0 +1,26 @@
+"""
+IRODB - A Python database library with .irodb format and hash-based indexing
+"""
+
+from .core import IRODB
+from .hash_system import HashManager
+from .index import IndexManager
+from .transaction import TransactionManager
+from .utils import DatabaseUtils
+from .exceptions import IRODBError, HashError, IntegrityError
+from .constants import VERSION, PAGE_SIZE, MAGIC_HEADER
+
+__version__ = "1.0.8"
+__all__ = [
+    'IRODB',
+    'HashManager',
+    'IndexManager',
+    'TransactionManager',
+    'DatabaseUtils',
+    'IRODBError',
+    'HashError',
+    'IntegrityError',
+    'VERSION',
+    'PAGE_SIZE',
+    'MAGIC_HEADER'
+]
